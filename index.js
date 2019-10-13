7e3a483d51f0ac99342d75f8a0321db2840ad316
sana <sanabenhendalaajimi@gmail.com>
2019-10-13 15:48:42 +0100

    code corrigé

--- index.js
+++ index.js
@@ -4,49 +4,16 @@ let pets = [
   { name: "Jasper", type: "dog", bornOn: 2016 }
 ];
 
-/*  function getAge(pet) {
-  return new Date().getFullYear() - pet.bornOn;
-}*/
 const getAge = pet => new Date().getFullYear() - pet.bornOn;
-//let petsWithAge = [];
-/*for (var i = 0; i < pets.length; i++) {
-  var pet = pets[i];
-  pet.age = getAge(pet);
 
-  petsWithAge.push(pet);
-}
-*/
 let petsWithAge = pets.map(pet =>{
   pet.age = getAge(pet);
-    //console.log(pet);
    return pet;
 });
+console.log(petsWithAge);
 
-console.log(petsWithAge, 'ggg');
-
-/*let dogs = [];
-for (var i = 0; i < pets.length; i++) {
-  var pet = pets[i];
-  if (pet.type === "dog") {
-    dogs.push(pet);
-  }
-}*/
-//first essay
-//let dogs = pets.filter(pet=>{return pet.type === "dog"});
 let dogs = pets.filter(pet=>(pet.type === "dog"));
+console.log(dogs);
 
-
-
-console.log(dogs, 'rr');
-
-/*var jasper;
-for (var i = 0; i < pets.length; i++) {
-  var pet = pets[i];
-  if (pet.name === "Jasper") {
-    jasper = pet;
-  }
-}*/
 let jasper = pets.find(pet =>  (pet.name === "Jasper"));
-
-console.log(jasper, 'oo');
 console.log(`Jasper is  ${jasper.age}  years old`);
